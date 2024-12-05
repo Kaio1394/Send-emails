821319a3fd314cc1bd72071fce7686ba148c426a
Kaio <kaio.santiago.13@hotmail.com>
2024-12-05 10:30:46 -0300

    add libs, cpp e headers files

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include "sendemailspage.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/icons/Email.ico"));
-
     SendEmailsPage w;
     w.setWindowTitle("Send Emails");
     w.show();
